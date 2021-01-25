06028331b740ca0788aad7552e353556351115b3
Zarovniy Serhiy <zarovniyserhiy1991@gmail.com>
2021-01-25 10:03:06 +0100

    Working with async function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Fuck this e")
+
+const getResponse = async (url) => {
+    const res = await fetch (url);
+    const body = await res.json();
+    return body;
+}
+
+getResponse ('https://swapi.dev/api/people/1/')
+.then((body)=>{
+    console.log(body)
+})
+
+
+
+
