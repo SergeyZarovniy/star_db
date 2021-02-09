d8168f4642baf1307d3086e9ec95ebb94122ace9
Zarovniy Serhiy <zarovniyserhiy1991@gmail.com>
2021-02-09 21:57:17 +0100

    Add button for exp

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import "./app.css";
 import Header from '../header/header';
 import RandomPlanet from '../random-planet/random-planet';
@@ -10,16 +10,40 @@ import Spinner from "../spinner/spinner";
 
 
 
-const App = () => {
-return(
-    <div>
+export default class App extends Component {
+
+  state = {
+    showRandomPlanet : true
+  };
+
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      }
+    });  
+  };
+
+render () {
+
+  const planet = this.state.showRandomPlanet ? <RandomPlanet/> : null;
+
+  return(
+    <div className='stardb-app'>
         <Header/>
-        <RandomPlanet/>
+
+        {planet}
+
+        <button 
+            className="toggle-planet btn btn-warning btn-lg" 
+            onClick={this.toggleRandomPlanet}>
+            Toggle Random Planet
+        </button>
         <div className="row mb2">
-        <div className="col-md-5 g-3">
+        <div className="col-md-6 g-3">
           <ItemList />
         </div>
-        <div className="col-md-5 g-3">
+        <div className="col-md-6 g-3 ">
           <PersonDetails />
         </div>
         
@@ -27,5 +51,5 @@ return(
     </div>
 )
 }
+}
 
-export default App;
